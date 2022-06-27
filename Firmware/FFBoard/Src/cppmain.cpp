41af87783fd1ee2e05db21c570726a9ee14f8bf5
Marc Paquette <5941665+MarkzP@users.noreply.github.com>
2022-06-26 23:15:04 -0400

    wip

--- Firmware/FFBoard/Src/cppmain.cpp
+++ Firmware/FFBoard/Src/cppmain.cpp
@@ -6,7 +6,9 @@
 #include "cpp_target_config.h"
 #include "cmsis_os.h"
 
-#ifdef STM32F407xx
+#ifdef STM32H743xx
+#include "stm32h7xx_hal_flash.h"
+#else
 #include "stm32f4xx_hal_flash.h"
 #endif
 
@@ -55,7 +57,9 @@ void cppmain() {
 	// Flash init
 	// TODO verify why or if flash does not erase or initialize correctly on some new chips
 	HAL_FLASH_Unlock();
-#ifdef STM32F407xx
+#ifdef STM32H743xx
+	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGSERR | FLASH_FLAG_WRPERR);
+#else
 	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR | FLASH_FLAG_BSY);
 #endif
 
